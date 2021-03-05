3098519bbc7486df93986512c76f388c1890ae41
noahkay28 <76534478+noahkay28@users.noreply.github.com>
2021-03-05 14:37:52 -0500

    Update switch.py

--- switch.py
+++ switch.py
@@ -7,16 +7,25 @@ orange = Button(21, pull_up=False)
 
 while True:
     if red.is_pressed:
-        print("red")
+        if blue.value == 0 and orange.value == 0:
+            print("red")
     if blue.is_pressed:
-        print("blue")
+        if red.value == 0 and orange.value == 0:
+            print("blue")
     if orange.is_pressed:
-        print("orange")
-     if orange.is_pressed and red.is_pressed:
-        print("Orange and Red Pressed")
+        if red.value == 0 and blue.value == 0:
+            print("orange")
+    if orange.is_pressed and red.is_pressed:
+        if blue.value == 0:
+            print("Orange and Red Pressed")
     if orange.is_pressed and blue.is_pressed:
-        print("Orange and Blue Pressed")
+        if red.value == 0:
+            print("orange and blue")
     if blue.is_pressed and red.is_pressed:
-        print("Blue and Red Pressed")
-    else:
+        if orange.value == 0:
+            print("blue and red")
+    if blue.is_pressed and red.is_pressed and orange.is_pressed:
+        print("everything")
+    if blue.value == 0 and red.value == 0 and orange.value == 0:
         print("Nothing Pressed")
+sleep(1)
