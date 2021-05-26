346870417f208df167745d0c0e63a03f508bf54b
noahkay28 <76534478+noahkay28@users.noreply.github.com>
2021-05-26 14:54:03 -0400

    Update mostiure_calibration.py

--- mostiure_calibration.py
+++ mostiure_calibration.py
@@ -3,6 +3,7 @@ import numpy as np #library for performing math in your code
 import board
 import busio
 from time import sleep
+from gpio import LED
 i2c = busio.I2C(board.SCL, board.SDA)
 
 #library for communicating with the ADS1115
@@ -12,18 +13,52 @@ from adafruit_ads1x15.analog_in import AnalogIn
 #Create an object from the ADS library
 ads = ADS.ADS1115(i2c)
 
-raw_reading0 = AnalogIn(ads, ADS.P0) #take a raw reading from the ADS1115 on channel 0 (first channel)
-raw_reading1 = AnalogIn(ads, ADS.P1) #take a raw reading from the ADS1115 on channel 0 (first channel)
-raw_reading2 = AnalogIn(ads, ADS.P2) #take a raw reading from the ADS1115 on channel 0 (first channel)
-raw_reading3 = AnalogIn(ads, ADS.P3) #take a raw reading from the ADS1115 on channel 0 (first channel)
-
+relay = LED(17)
+num_reading = 20
 volumes = [200.0, 175.0, 150.0, 125.0, 100.0, 75.0, 50.0, 25.0, 0.0]
 values = [8832.0, 8984.0, 9176.25, 9284.0, 9420.0, 9258.0, 9496.0, 9900.0, 18080.0]
+
+ 
 while True:
-    calc = np.interp(raw_reading0.value, values, volumes)#interpert the values and have it rint how much water is in
-    print(calc)
-    print("Sensor 0:", raw_reading0.value)
-    print("Sensor 1:", raw_reading1.value)
-    print("Sensor 2:", raw_reading2.value)
-    print("Sensor 3:", raw_reading3.value)
-    sleep(3)
+    total0 = 0
+    total1 = 0
+    total2 = 0
+    total3 = 0
+    
+    for i in range(num_reading):
+        raw_reading0 = AnalogIn(ads, ADS.P0)
+        raw_reading1 = AnalogIn(ads, ADS.P1)
+        raw_reading2 = AnalogIn(ads, ADS.P2)
+        raw_reading3 = AnalogIn(ads, ADS.P3)
+        total0+= raw_reading0.value
+        total1+= raw_reading1.value
+        total2+= raw_reading2.value
+        total3+= raw_reading3.value
+    
+    total0/= num_reading
+    total1/= num_reading
+    total2/= num_reading
+    total3/= num_reading
+
+    total_average = (total0 + total1 + total2 + total3) / 4.0
+    calct = np.interp(total_average, values, volumes)
+    calc0 = np.interp(total0, values, volumes)#interpert the values and have it rint how much water is in
+    calc1 = np.interp(total1, values, volumes)
+    calc2 = np.interp(total2, values, volumes)
+    calc3 = np.interp(total3, values, volumes)
+    print("Total Average Moisture:", calct)
+    print("Sensor 0 Moisture:", calc0)
+    print("Sensor 1 Moisture:", calc1)
+    print("Sensor 2 Moisture:", calc2)
+    print("Sensor 3 Moisture:", calc3)
+    print("RR Sensor 0:", raw_reading0.value)
+    print("RR Sensor 1:", raw_reading1.value)
+    print("RR Sensor 2:", raw_reading2.value)
+    print("RR Sensor 3:", raw_reading3.value)
+    
+    if calct < 20:
+        relay.on()
+        sleep (57)
+        relay.off()
+    sleep(60)
+    
