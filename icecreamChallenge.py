f0016ddf4abd319093170171a425bab555c9eff9
noahkay28 <76534478+noahkay28@users.noreply.github.com>
2021-11-12 09:26:41 -0500

    Update icecreamChallenge.py

--- icecreamChallenge.py
+++ icecreamChallenge.py
@@ -8,10 +8,12 @@ flavorChoice = guizero.ButtonGroup(app, options=['chocolate', 'vanilla', 'strawb
 sliderTitle = guizero.Text(app, text = 'Number of Scoops')
 slider = guizero.Slider(app)
 dairyFree = guizero.CheckBox(app, text = 'Dairy Free')
-if dairyFree == true:
-    text = guizero.Text(app, text = 'YOUR WEIRD')
-toppingsTitle = guizero.Text(app, text = 'Extra Toppings?')
-textbox = guizero.TextBox(app)
-
+toppingsTitle = guizero.Text(app, text = 'Extra Toppings')
+toppingsList = guizero.ListBox(app, items=['nuts', 'chocolate sauce', 'wafer'])
+costDetails = guizero.Text(app, text = 'Cost is $2 plus $.50 per topping') 
+finalCost = guizero.TextBox(app)
+def buttonClicked():
+    info('Your Order Has Been Placed - Thank You')
+finishedButton = guizero.PushButton(app, command = buttonClicked, text = 'Place Order')
 
 app.display()
